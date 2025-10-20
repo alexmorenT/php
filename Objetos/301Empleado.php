ebaa7a3202cb8c85b09fc17b6e89cca3a81837df
Alexander <amoretroco@gmail.com>
2025-10-20 13:09:04 +0200

    mensaje de prueba

--- Objetos/301Empleado.php
+++ Objetos/301Empleado.php
@@ -1,65 +1,73 @@
 <?php
 
-class Empleado {
+class Empleado
+{
     private string $nombre;
     private string $apellido;
     private int $sueldo;
 
-        public function __construct(string $nom, string $ape, int $sueldo){
-            $this->nombre = $nom;
-            $this->apellido = $ape;
-            $this->sueldo = $sueldo;
-        }
+    public function __construct(string $nom, string $ape, int $sueldo)
+    {
+        $this->nombre = $nom;
+        $this->apellido = $ape;
+        $this->sueldo = $sueldo;
+    }
 
-        public function setNombre(string $nom){
-            $this->nombre = $nom;
-        }
+    public function setNombre(string $nom)
+    {
+        $this->nombre = $nom;
+    }
 
-        public function setApellido(string $ape) {
-            $this->apellido = $ape;
-        }
+    public function setApellido(string $ape)
+    {
+        $this->apellido = $ape;
+    }
 
-        public function setSueldo(int $sueldo) {
-            $this->sueldo = $sueldo;
-        }
+    public function setSueldo(int $sueldo)
+    {
+        $this->sueldo = $sueldo;
+    }
 
-        public function getNombre () : string {
-            return $this->nombre;
-        }
+    public function getNombre(): string
+    {
+        return $this->nombre;
+    }
 
-         public function getApellido () : string {
-            return $this->apellido;
-        }
+    public function getApellido(): string
+    {
+        return $this->apellido;
+    }
 
-         public function getSueldo () : int {
-            return $this->sueldo;
-        }
+    public function getSueldo(): int
+    {
+        return $this->sueldo;
+    }
 
-        public function getNombreCompleto() : string {
-            return $this->getNombre() . " " . $this->getApellido();
-        }
+    public function getNombreCompleto(): string
+    {
+        return $this->getNombre() . " " . $this->getApellido();
+    }
 
-        public function debePagarImpuestos() : bool {
-            if ($this->sueldo > 3333) {
-                return true;
-            } else {
-                return false;
-            }
+    public function debePagarImpuestos(): bool
+    {
+        if ($this->sueldo > 3333) {
+            return true;
+        } else {
+            return false;
         }
+    }
 }
+// holaaaa
 
+$emple1 = new Empleado("Miguel", "Hernandez", 3336);
 
-    $emple1 = new Empleado ("Miguel", "Hernandez", 3336);
+echo $emple1->getNombreCompleto();
+echo "<br>";
+echo $emple1->debePagarImpuestos();
+echo "<br>";
 
-    echo $emple1->getNombreCompleto();
-    echo "<br>";
-    echo $emple1->debePagarImpuestos();
-     echo "<br>";
+$emple2 = new Empleado("Victor", "Guerrero", 2050);
 
-    $emple2 = new Empleado ("Victor", "Guerrero", 2050);
-
-   echo $emple2->getNombreCompleto();
-    echo "<br>";
-   echo $emple2->debePagarImpuestos();
-
-?>
+echo $emple2->getNombreCompleto();
+echo "<br>";
+echo $emple2->debePagarImpuestos();
