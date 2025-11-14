bea002545a39ae7cf86412b9d34b42325861aa89
alexmorenT <amoretroco@gmail.com>
2025-11-14 14:13:18 +0100

    Update 406contadorVisitas.php

--- CookiesySesiones/406contadorVisitas.php
+++ CookiesySesiones/406contadorVisitas.php
@@ -1,1 +1,21 @@
+<form method="GET" action="406contadorVisitas.php">
+    Resetear <input type="submit" name="reset" value="reset" />
+</form>
+
 <?php
+
+$reinicio = false;
+
+if (isset($_GET["reset"])) {
+    setcookie("contadorVisitas", "", time() - (60 * 60 * 24 * 30));
+    $reinicio = true;
+}
+
+if (isset($_COOKIE["contadorVisitas"]) && $reinicio == false) {
+    $accesos = $_COOKIE["contadorVisitas"];
+    $accesos++;
+    setcookie("contadorVisitas", $accesos, time() + (60 * 60 * 24 * 30));
+} else {
+    $accesos = 1;
+    setcookie("contadorVisitas", $accesos, time() + (60 * 60 * 24 * 30));
+}
